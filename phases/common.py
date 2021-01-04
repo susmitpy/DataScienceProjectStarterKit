8266782850e37b39a2d567feaba2ed4af348de37
Susmit Vengurlekar <susmit.py@gmail.com>
2021-01-04 11:30:00 +0530

    plots

--- phases/common.py
+++ phases/common.py
@@ -1,7 +1,12 @@
 from IPython.display import display_html
 
-def display_side_by_side(*args,mssg=None):
+def display_side_by_side(mssg=None,*args):
     html_str=''
-    for df in args:
-        html_str+=df.to_html()
-    display_html(html_str.replace('table','table style="display:inline;padding:10px;margin:5px"'),raw=True)
+    for index,df in enumerate(args):
+        if mssg:
+            html_str+=df.to_html().replace("<thead",f"<caption>{mssg[index]}</caption>\n<thead")
+        else:
+            html_str+=df.to_html()
+        
+        
+    display_html(html_str.replace('table','table style="display:inline;padding:12px;margin:6px;padding-top:10px;"'),raw=True)
